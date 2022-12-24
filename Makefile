b0f2f12dffd12fe6616397eda4adb2b33f13d645
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-25 02:55:09 +0700

    Add pys

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	uvicorn main:app --reload --port 8080
+
+env:
+	venv\Scripts\activate
---
+++ main.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from starlette.config import Config
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+import requests
+import json
+import os
+from datetime import datetime
+
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
+
+from starlette.responses import RedirectResponse, JSONResponse
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        os.makedirs('output')
+    except:
+        pass
+
+origins = [
+  "*"
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["POST", "GET"],
+  allow_headers=["*"],
+)
+
+@app.post("/")
+async def send_msg(request: Request):
+    try:
+        payload = await request.json()
+        from_address = (request.client.__str__())
+        headers = (request.headers.__str__())
+        print(datetime.timestamp(datetime.now()))
+        with open(f'output/{datetime.timestamp(datetime.now())}.txt', 'w') as output_buffer:
+            print(payload, file=output_buffer)
+            print(from_address, file=output_buffer)
+            print(headers, file=output_buffer)
+            # output_buffer.write(payload)
+        return JSONResponse({"message": "200"})
+    except:
+        return JSONResponse({"message": "500"})
